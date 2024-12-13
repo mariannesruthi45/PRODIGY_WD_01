3bf01049f1bbf638cb328878b5e1021120ac53bf
Marianne Sruthi <156935047+mariannesruthi45@users.noreply.github.com>
2024-12-13 19:39:16 +0530

    Add files via upload

---
+++ nav2.js
@@ -0,0 +1,13 @@
+// Change navbar background on scroll
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+  });
+  
+  
+  
+  
